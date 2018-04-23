44b76529666cb66fb7d59829284f9a1fde8860fb
leon <jianliangzhang@ctrip.com>
2018-04-23 17:31:24 +0800

    props

--- src/app.jsx
+++ src/app.jsx
@@ -3,11 +3,15 @@ import { div, input, p } from '@cycle/dom'
 
 const App = sources => {
 
+  const props$ = sources.props
+
   // 点击事件
   const $click = sources.DOM.select('a').events('click')
   const $clickDecrement = sources.DOM.select('.dec').events('click').mapTo(-1)
   const $clickIncrement = sources.DOM.select('.inc').events('click').mapTo(+1)
-  const $toggleClick = $click.fold(x => !x).startWith(false)
+  const $toggleClick = props$.map(({ toggled }) => 
+    $click.fold(x => !x).startWith(toggled)
+  ).flatten()
 
   // 请求
   const getRandomUser$ = $click.map(() => {
@@ -19,33 +23,36 @@ const App = sources => {
     }
   })
 
-
   // 累加
   const action$ = xs.merge(
     $clickDecrement,
     $clickIncrement
   )
 
-  const count$ = action$.fold((x, y) => x + y, 0)
+  const count$ = props$.map(({ count }) => 
+    action$.fold((x, y) => x + y, count)
+  ).flatten()
 
+  // combined
   const combined$ = xs.combine(count$, $toggleClick)
 
   // dom变化
   const vdom$ = combined$
-    .map(([count, toggled]) => 
-      <div>
+    .map(([count, toggled]) => {
+      return (
         <div>
-          <a>button</a>
-          <p>{toggled ? 'ON' : 'OFF'}</p>
+          <div>
+            <a>button</a>
+            <p>{toggled ? 'ON' : 'OFF'}</p>
+          </div>
+          <div>
+            <botton className="dec">Decrement</botton>
+            <botton className="inc">Increment</botton>
+            <p>Counter: {count}</p>
+          </div>
         </div>
-        <div>
-          <botton className="dec">Decrement</botton>
-          <botton className="inc">Increment</botton>
-          <p>Counter: {count}</p>
-        </div>
-      </div>
-    )
-
+      )
+    })
 
   return {
     DOM: vdom$,
--- src/index.js
+++ src/index.js
@@ -2,10 +2,15 @@ import { run } from '@cycle/run'
 import { makeDOMDriver } from '@cycle/dom'
 import { App, HTTP } from './app'
 import { makeHTTPDriver } from '@cycle/http'
+import xs from 'xstream'
 
 const main = App
 
 const drivers = {
+  props: () => xs.of({
+    toggled: false,
+    count: 0
+  }),
   DOM: makeDOMDriver('#root'),
   HTTP: makeHTTPDriver()
 }
